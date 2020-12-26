498ac9f2fdeafe4e68767c9815c93edf9031d438
Tobias Kircher <tobytoby1212@hotmail.com>
2020-12-26 16:28:48 +0000

    Added create recipe feature.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -28,7 +28,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     )
     tags = serializers.PrimaryKeyRelatedField(
         many=True,
-        queryset=Ingredient.objects.all()
+        queryset=Tag.objects.all()
     )
 
     class Meta:
